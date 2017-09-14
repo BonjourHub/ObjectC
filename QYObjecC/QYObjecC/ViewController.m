79bba397a3b823f6f1fb62cd413f15f466066d65
pengfei28 <pengfei28@staff.weibo.com>
2017-09-14 18:39:56 +0800

    workspace建立 加入了一个新项目

---
+++ QYObjecC/QYObjecC/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  QYObjecC
+//
+//  Created by pengfei28 on 2017/9/14.
+//  Copyright © 2017年 bj-m-mojixiaowei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ QYObjecC/QYObjecC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QYObjecC
+//
+//  Created by pengfei28 on 2017/9/14.
+//  Copyright © 2017年 bj-m-mojixiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
